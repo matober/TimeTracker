72827e011637740c16fb19c44e80ad4f87ec3913
ANDY VUONG <andvuo@gmail.com>
2017-02-08 21:25:16 -0500

    User_and_Activity_Database
    Add preliminary attributes and validations for the user and activity
    databases. Will test when the chance is available. Postgre was the
    database used in this update.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,14 @@
 class User < ApplicationRecord
 
-  attr_accessor :password
-  validates :username, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
-  validates :first_name, :presence => true
-  validates :last_name, :presence => true
-  validates :email, :presence => true, :uniqueness => true, length: { minimum: 5}
-  validates :password, :confirmation => true
-  validates_length_of :password, minimum: 5, :on => create
+    before_save { self.email = email.downcase }
+    #EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+    validates :first_name, presence: true, length: {minimum: 1}
+    validates :last_name, presence: true, length: {minimum: 1}
+    validates :email, presence: true, uniqueness: true, legnth: {minimum: 5}
+    validates :username, presence: true, uniqueness: true, length: {minimum: 1}
+    validates :password_digest, length: {minimum: 6}
 
   has_secure_password
-
-
 
 end
