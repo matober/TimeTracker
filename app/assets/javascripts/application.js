8318733f85f63994c2f6f004371d8d54d49c0d34
Kristopher Trevino <trevinkr@mail.gvsu.edu>
2017-03-23 17:54:58 -0400

    Update 3/23/17

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,9 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
+//= require jquery
+//= require jquery-ui
+//= require jquery_ujs
+//= require turbolinks
+//= require bootstrap-sprockets
+//= require_tree.
