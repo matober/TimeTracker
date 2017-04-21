7f733392a2ce07f367656a07d461cff6e3d616b1
Kristopher Trevino <trevinkr@mail.gvsu.edu>
2017-04-21 07:16:28 -0400

    Fixed icons layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,4 +8,11 @@ class HomeController < ApplicationController
     @category = Category.new
   end
 
+  def sort
+    params[:order].each do |key, value|
+      Activity.find(value[:id]).update_attribute(:priority, value[:position])
+    end
+    render :nothing => true
+  end
+
 end
