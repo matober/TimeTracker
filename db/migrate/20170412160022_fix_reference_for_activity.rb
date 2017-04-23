42ec44e291c5626a05816d1a79c54dce47e4168a
Maxwell Tober <toberm@mail.gvsu.edu>
2017-04-23 16:44:22 -0400

    Fixing heroku

--- db/migrate/20170412160022_fix_reference_for_activity.rb
+++
@@ -1,8 +0,0 @@
-class FixReferenceForActivity < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :activities, :category_id, foreign_key: false
-    add_reference :activities, :categories, foreign_key: true
-
-
-  end
-end
