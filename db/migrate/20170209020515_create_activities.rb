74b1154941820c21bc98acb35790bbbc0c391140
Maxwell Tober <toberm@mail.gvsu.edu>
2017-04-23 16:24:58 -0400

    More heroku stuff

---
+++ db/migrate/20170209020515_create_activities.rb
@@ -0,0 +1,15 @@
+class CreateActivities < ActiveRecord::Migration[5.0]
+  def change
+    create_table :activities do |t|
+
+      t.integer :total_time
+      t.string :name
+      t.boolean :hidden
+      t.datetime :date
+
+      t.timestamps
+    end
+  end
+
+
+end
