b412bb74d840d31a4555d6e00c5cf2f0a9bf66b6
Maxwell Tober <toberm@mail.gvsu.edu>
2017-04-23 16:26:51 -0400

    Deleted Migration

--- db/migrate/20170310031418_set_more_defaults.rb
+++
@@ -1,9 +0,0 @@
-class SetMoreDefaults < ActiveRecord::Migration[5.0]
-  def self.up
-    change_column :activities, :hidden, :boolean, :default => FALSE
-  end
-
-  def self.down
-    raise ActiveRecord::IrreversibleMigration, "Can't remove the default"
-  end
-end
