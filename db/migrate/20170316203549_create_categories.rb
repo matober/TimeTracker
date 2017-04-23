2983cae81d234ab8d0b33fd09adf4351756a5599
Maxwell Tober <toberm@mail.gvsu.edu>
2017-04-23 18:09:56 -0400

    Fix Migration

--- db/migrate/20170316203549_create_categories.rb
+++ db/migrate/20170316203549_create_categories.rb
@@ -1,8 +1,5 @@
 class CreateCategories < ActiveRecord::Migration[5.0]
   def change
-    create_table :categories do |t|
-      t.integer :user_id
-      t.timestamps
-    end
+    add_reference :categories, :users, foreign_key: true
   end
 end
---
+++ db/migrate/20170423220910_add_user_to_category.rb
@@ -0,0 +1,4 @@
+class AddUserToCategory < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
