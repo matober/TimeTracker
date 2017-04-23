bd7513d21c5ad99544267e35b3381329ec8cbda0
Maxwell Tober <toberm@mail.gvsu.edu>
2017-04-23 16:54:11 -0400

    Fixing migrations

--- db/migrate/20170212013914_create_users.rb
+++ db/migrate/20170212013914_create_users.rb
@@ -1,11 +1,16 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-
       t.string :email
       t.string :password
       t.string :password_hash
       t.string :password_salt
+      t.datetime :created_at
+      t.datetime :updated_at
+      t.string :persistence_token
+      t.string :crypted_password
+      t.string :first_name
+      t.string :last_name
       t.timestamps
     end
   end
